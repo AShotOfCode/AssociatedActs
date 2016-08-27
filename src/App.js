b9f0ea5d548266a32c977be83bff00e1d8a0bc3e
Erkan Sen <erkansen14@hotmail.com>
2016-08-27 15:13:59 -0500

    Render names of J.Cole associated acts in react

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import $ from 'jquery'
 
+let title = "J._Cole"
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+      super(props)
+      this.state = {
+        rappers: [],
+      }
+    }
+    componentDidMount(){
+      let tempRappers = []
+      let url = "http://dbpedia.org/sparql"
+      let query = "select ?z where {<http://dbpedia.org/resource/" + title + "> dbo:associatedMusicalArtist ?z}"
+      let queryUrl = encodeURI(url + "?query=" + query + "&format=json")
+      $.get(queryUrl).then(data => {
+              for (let value of data.results.bindings){
+                  tempRappers.push(value.z.value.split("http://dbpedia.org/resource/")[1])
+                }
+          }).then(data => {
+            this.setState({
+              rappers: tempRappers
+            })
+          })
+    }
+    getRappers() {
+        return (
+            <ul>
+                {this.state.rappers.map(function(name, index){
+                    return <li key={ index }>{name}</li>;
+                  })}
+            </ul>
+        )
+    }
+    render() {
+        return (
+            <div className="App">
+                {this.getRappers()}
+            </div>
+        );
+    }
 }
 
 export default App;
