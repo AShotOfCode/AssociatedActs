12d087c4ea8443ef000bcd80804b26cca85e817e
Erkan Sen <erkansen14@hotmail.com>
2016-08-28 01:45:53 -0500

    imported J.Cole image

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            rappers: [],
+            rappers: ["nodes": []],
             names: {"nodes" : [{name: 'Erkan', group: 1}, {name: 'Jophy', group: 1}, {name: 'Aaron', group: 1}, {name: 'Lauren', group: 2}]},
             links: {"links" : [
                     {
@@ -20,7 +20,7 @@ class App extends Component {
                         "target": 0,
                         "value": 2
                     }, {
-                        "source": 1,
+                        "source": 0,
                         "target": 2,
                         "value": 2
                     }
@@ -30,21 +30,32 @@ class App extends Component {
         }
     }
     componentDidMount() {
-        let tempRappers = []
+        let tempRappers = {"nodes": []}
         let url = "http://dbpedia.org/sparql"
         let query = "select ?z where {<http://dbpedia.org/resource/" + title + "> dbo:associatedMusicalArtist ?z}"
+        let thumbnail =  "select ?z where {<http://dbpedia.org/resource/" + title + "> dbo:thumbnail ?z}"
         let queryUrl = encodeURI(url + "?query=" + query + "&format=json")
+        tempRappers["nodes"].push({name: title, group: 1})
         $.get(queryUrl).then(data => {
             for (let value of data.results.bindings) {
-                tempRappers.push(value.z.value.split("http://dbpedia.org/resource/")[1])
+                tempRappers["nodes"].push({name: value.z.value.split("http://dbpedia.org/resource/")[1], group: 1})
             }
+        }).then(data => {
+            this.setState({rappers: tempRappers})
+        }).then(data => {
+        queryUrl = encodeURI(url + "?query=" + thumbnail + "&format=json")
+        $.get(queryUrl).then(data => {
+            tempRappers.nodes[0].icon = data.results.bindings[0].z.value
+            tempRappers.nodes[0].radius = 5
+            console.log(tempRappers.nodes)
         }).then(data => {
             this.setState({rappers: tempRappers, loaded: true})
         })
+      })
     }
     drawD3() {
       console.log(this.state)
-        let names = this.state.names
+        let names = this.state.rappers
         let links = this.state.links
         //d3 function to draw
         var width = 960
@@ -55,7 +66,11 @@ class App extends Component {
         force.nodes(names.nodes).links(links.links).start();
         var link = svg.selectAll(".link").data(links.links).enter().append("line").attr("class", "link");
         var node = svg.selectAll(".node").data(names.nodes).enter().append("g").attr("class", "node").call(force.drag);
-        node.append("image").attr("xlink:href", "https://github.com/favicon.ico").attr("x", -8).attr("y", -8).attr("width", 16).attr("height", 16);
+        node.append("svg:circle")
+            .attr("cursor","pointer")
+            .style("fill","#c6dbef")
+            .attr("r", "20px")
+        node.append("image").attr("xlink:href", function(d){return d.icon}).attr("x", -8).attr("y", -8).attr("width", 32).attr("height", 32);
         node.append("text").attr("dx", 12).attr("dy", ".35em").text(function(d) {
             return d.name
         });
